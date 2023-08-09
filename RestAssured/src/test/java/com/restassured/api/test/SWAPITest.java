e7ce82c16e1ff4ea1b69663a73d6e1f62a9b0fd5
ricokali <ricokali@ricokali.com>
2023-08-09 18:12:43 -0400

    update

--- RestAssured/src/test/java/com/restassured/api/test/SWAPITest.java
+++ RestAssured/src/test/java/com/restassured/api/test/SWAPITest.java
@@ -1,5 +1,45 @@
 package com.restassured.api.test;
 
-public class SWAPITest {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.hamcrest.Matchers;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.google.gson.Gson;
+
+import org.testng.annotations.Ignore;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class SWAPITest {
+	private static final Logger log = LogManager.getLogger(SWAPITest.class);
+	private final static String BASEURI = "https://swapi.dev/api/"; //1
+	private final static String BASEPATH = "people";
+	
+	@Test	
+	public void statusTest() {	
+		log.info("Status Test Running");		
+		Response response = 	
+		RestAssured	
+		.given()
+			.contentType(ContentType.JSON)	
+			.baseUri(BASEURI+"people")	
+		.when()
+			.get()
+		.then()
+			.assertThat()
+			.statusCode(200)
+			.statusLine("HTTP/1.1 200 OK")
+			.header("Content-Type", "application/json")
+		.extract()
+			.response();		
+	}
 }
