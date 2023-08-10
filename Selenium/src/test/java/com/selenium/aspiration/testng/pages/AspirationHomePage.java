e4d9e8a09566b01e155cc3eb5f372a254e8805f1
ricokali <ricokali@ricokali.com>
2023-08-10 15:39:37 -0400

    Selenium added

---
+++ Selenium/src/test/java/com/selenium/aspiration/testng/pages/AspirationHomePage.java
@@ -0,0 +1,5 @@
+package com.selenium.aspiration.testng.pages;
+
+public class AspirationHomePage {
+
+}
---
+++ Selenium/src/test/java/com/selenium/aspiration/testng/tests/TestBase.java
@@ -0,0 +1,5 @@
+package com.selenium.aspiration.testng.tests;
+
+public class TestBase {
+
+}
